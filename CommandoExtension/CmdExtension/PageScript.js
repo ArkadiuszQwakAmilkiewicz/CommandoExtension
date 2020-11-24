46f318fcaf5e5274e5650e9aac8d0b28488a82e2
TheRainbowKing <AAAonlinesales@outlook.com>
2020-11-24 23:16:03 +0000

    added overlay

--- CommandoExtension/CmdExtension/PageScript.js
+++ CommandoExtension/CmdExtension/PageScript.js
@@ -1,8 +1,20 @@
 console.log("PageScript runs");
 //Adding listener
 document.addEventListener('keydown', keyPressed);
-const searchOverlayWindow = document.createElement("box");
+const searchOverlayWindow = document.createElement("iframe");
+searchOverlayWindow.style =    
+            `
+            all: initial !important;
+            position: fixed !important;
+            top: 0 !important;
+            left: 0 !important;
+            width: 100vw !important;
+            height: 100vh !important;
+            z-index: 1000000 !important;
+            `;
+
 searchOverlayWindow.src = browser.extension.getURL("box.html");
+searchOverlayWindow.onload = Show;
 
 function keyPressed(e){
     
@@ -10,6 +22,16 @@ function keyPressed(e){
     if(e.keyCode > 64 && e.keyCode  < 91 && e.target.nodeName != "INPUT" && e.target.nodeName !="SELECT" && e.target.nodeName != "TEXTAREA"){
         console.log(e.key);
         //CODE
-        searchOverlayWindow.focus();
+        document.body.appendChild(searchOverlayWindow);
     }
+}
+
+function Show(){
+    const window = searchOverlayWindow.contentWindow;
+    const doc = searchOverlayWindow.contentDocument;
+    const searchBox = doc.getElementById("commandoBox");
+
+    window.focus();
+    searchBox.focus();
+
 }
